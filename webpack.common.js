62d39f78bf2a5ac64152d8511122ef0c7c012a43
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-11-03 12:23:44 +0530

    Configure webpack and create directories

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    
+    entry: {
+        app:'.src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
