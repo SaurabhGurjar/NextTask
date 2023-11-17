2c423e5ee0d304e6a44677117977d7bfeb3bb531
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-11-18 01:51:29 +0530

    Add: Projects and teams section to sidebar.

---
+++ src/modules/data.js
@@ -0,0 +1,140 @@
+const projects = [
+    {
+        name: 'Shopping Cart',
+        teams: [
+            {
+                name: 'Frontend',
+                subproject: 'UI design',
+                tasks: [
+                    {
+                        task: 'Design Navigation',
+                        open: '18-11-23',
+                        closed: '20-11-23',
+                        assignee: 'Rajat Morya',
+                        assignor: 'Saurabh Choudhary',
+                        status: 'Not started',
+                        priority: 'High',
+                    },
+                    {
+                        task: 'Sidebar Design',
+                        open: '18-11-21',
+                        closed: '20-11-23',
+                        assignee: 'Manav Badola',
+                        assignor: 'Saurabh Choudhary',
+                        status: 'Not started',
+                        priority: 'High',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: 'LLM system'
+    },
+    {
+        name: 'Data visualizer'
+    }
+];
+
+// class project {
+
+// }
+const teams = [
+    {
+        name: 'Web development',
+        projects: {
+            name: 'Shopping Cart',
+            teams: [
+                {
+                    name: 'Frontend',
+                    subproject: 'UI design',
+                    tasks: [
+                        {
+                            task: 'Design Navigation',
+                            open: '18-11-23',
+                            closed: '20-11-23',
+                            assignee: 'Rajat Morya',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                        {
+                            task: 'Sidebar Design',
+                            open: '18-11-21',
+                            closed: '20-11-23',
+                            assignee: 'Manav Badola',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                    ]
+                },
+                {
+                    name: 'Backend',
+                    subproject: 'User Authentication',
+                    tasks: [
+                        {
+                            task: 'Create API to authenticate user phone number.',
+                            open: '18-11-23',
+                            closed: '21-11-23',
+                            assignee: 'Hemant Yadav',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                        {
+                            task: 'Create API to authenticate user Email Address.',
+                            open: '18-11-23',
+                            closed: '21-11-23',
+                            assignee: 'Manish Shandilya',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                    ]
+                },
+
+                {
+                    name: 'Backend',
+                    subproject: 'Database Design',
+                    tasks: [
+                        {
+                            task: 'Create a Database to store user information.',
+                            open: '18-11-23',
+                            closed: '21-11-23',
+                            assignee: 'Arun singh',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                        {
+                            task: 'Create a Database to store products information.',
+                            open: '18-11-23',
+                            closed: '21-11-23',
+                            assignee: 'Kamal singh',
+                            assignor: 'Saurabh Choudhary',
+                            status: 'Not started',
+                            priority: 'High',
+                        },
+                    ]
+                },
+            ],
+        },
+
+    },
+    {
+        name: 'Artificial intelligence'
+    },
+
+    {
+        name: 'Data science'
+    },
+];
+
+export function getProjects() {
+    return projects;
+}
+
+export function getTeams() {
+    return teams;
+}
--- src/modules/ui-modules/sidebar.js
+++ src/modules/ui-modules/sidebar.js
@@ -1,14 +1,20 @@
-// Sidebar links
+// Import project and teams
+import { getProjects, getTeams } from '../data';
 
+// Import icons
 import inboxIcon from '../../assets/icons/inbox.svg';
 import privateTaskIcon from '../../assets/icons/private-task.svg';
 import folderIcon from '../../assets/icons/folder.svg';
 import helpIcon from '../../assets/icons/help.svg';
-import inviteIcon from '../../assets/icons/invite.svg';
+import addIcon from '../../assets/icons/invite.svg';
 import taskIcon from '../../assets/icons/task.svg';
 import todayIcon from '../../assets/icons/today.svg';
+import teamIcon from '../../assets/icons/team.svg';
 import upcomingIcon from '../../assets/icons/upcoming.svg';
+import ArrowIcon from '../../assets/icons/arrow.svg';
+import LOGO from '../../assets/icons/logo.svg';
 
+// Sidebar links
 const defaultTabs = [{
     name: 'Inbox',
     icon: inboxIcon,
@@ -19,7 +25,7 @@ const defaultTabs = [{
 {
     name: 'Today',
     icon: todayIcon,
-}, 
+},
 {
     name: 'Upcoming',
     icon: upcomingIcon,
@@ -34,25 +40,65 @@ function createIconElement(link) {
     return `<img src="${link.icon}" id="${link.name.toLowerCase()}-icon" class="sd-icons">`;
 }
 
-function createToplinksHtml(links) {
+function createLinksHtml(links) {
     let sdHtml = '';
     links.forEach((item) => {
         sdHtml += `<div class="sd-link-wrapper">${createIconElement(item) + createlink(item)}</div>`;
     });
     return sdHtml;
 }
-const projects = '';
-const teams = '';
+
+function createHeading(array) {
+    let headings = '';
+    array.forEach((item) => {
+        headings += (
+            `<div class="sd-projects-link-container">
+                <div class="sd-project-color-box"></div>
+                <a id="${item.name.toLowerCase()}" class="sd-links sd-projects-link" href="#">
+                    <span class="sd-projects-link-text" id="${item.name.toLowerCase()}">${item.name}</span>
+                </a>
+            </div>`
+        );
+    });
+    return headings;
+}
+
+function createTeamLinkHtml(array) {
+    let teams = '';
+    array.forEach((item) => {
+        teams += (
+            `<div class="sd-team-link-container">
+                <div class="sd-team-icon-wrapper">
+                    <span class="sd-team-icon">${item.name.toUpperCase()[0]}</span>
+                </div>
+                <a id="${item.name.toLowerCase()}" class="sd-links sd-team-link" href="#">
+                    <span class="sd-team-link-text" id="${item.name.toLowerCase()}">${item.name}</span>
+                </a>
+            </div>
+        `);
+    });
+    return teams;
+}
 
 const sidebarFooter = [{
     name: "Invite people",
-    icon: inviteIcon,
+    icon: addIcon,
 },
 {
     name: "Help",
     icon: helpIcon,
 }
-]
+];
+
+function parseData() {
+    const projects = getProjects();
+    projects.forEach((item) => {
+
+    });
+    console.log(getTeams());
+}
+
+parseData();
 
 // Sidebar contain two section top section and bottom section
 
@@ -61,27 +107,57 @@ const sidebarFooter = [{
 // Bottom section contains Invite tab links and help tap link
 
 export default function sidebar() {
-    return `
+    return (
+        `
         <div class="sidebar">
             <div class="top-section">
-                <div class="sd-header">
-                    <h4 id="logo">NextTask</h4>
+                <div class="logo-wrapper">
+                    <span id="logo-text">NextTask</span><img src="${LOGO}" id="logo-icon" alt="NT">
                 </div>
                 <div class="sd-main">
-                    <div class="sd-heading">
-                        ${createToplinksHtml(defaultTabs)}
+                    <div class="sd-section-wrapper">
+                        ${createLinksHtml(defaultTabs)}
                     </div>
-                    <div class="sd-heading" id="projects">
-                        <h5>Projects</h5>
+                    <div class="sd-section-wrapper">
+                        <div class="sd-heading-wrapper">
+                            <div class="sd-heading-text-icon">
+                                <img src="${folderIcon}" class="sd-icons">
+                                <span id="projects" class="sd-heading-text">Projects</span>
+                            </div>
+                            
+                            <div class="sd-buttons-interface-btn-wrapper">
+                                <button id="projects" class="interface-btn">
+                                    <img src="${addIcon}" class="sd-icons add-btn">
+                                </button>
+                                <button id="projects" class="dropdown-btn interface-btn">
+                                    <img src="${ArrowIcon}" class="sd-icons">
+                                </button>
+                            </div>
+                        </div>
+                        ${createHeading(getProjects())}
                     </div>
-                    <div class="sd-heading" id="Teams">
-                        <h5>Teams</h5>
+                    <div class="sd-section-wrapper">
+                        <div class="sd-heading-wrapper">
+                            <div class="sd-heading-text-icon">
+                                <img src="${teamIcon}" class="sd-icons">
+                                <span id="team" class="sd-heading-text">Team</span>
+                            </div>
+                            <div class="sd-buttons-interface-btn-wrapper">
+                                <button id="projects" class="interface-btn">
+                                    <img src="${addIcon}" class="sd-icons add-btn">
+                                </button>
+                                <button id="team" class="dropdown-btn interface-btn">
+                                    <img src="${ArrowIcon}" class="sd-icons">
+                                </button>
+                            </div>
+                        </div>
+                        ${createTeamLinkHtml(getTeams())}
                     </div>
                 </div>
             </div>
             <div class="sd-footer">
-                ${createToplinksHtml(sidebarFooter)}
+                ${createLinksHtml(sidebarFooter)}
             </div>
         </div>
-    `;
+    `);
 } 
