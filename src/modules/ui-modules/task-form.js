2082a316b94def1ae14b31de2b7596df39bf6f83
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-11-25 11:35:36 +0530

    Create task form

---
+++ src/modules/ui-modules/task-form.js
@@ -0,0 +1,36 @@
+import '../../css/task-form.css';
+import assigneeIcon from '../../assets/icons/person.svg';
+import dateIcon from '../../assets/icons/calendar.svg';
+import priorityIcon from '../../assets/icons/flag.svg';
+export default function taskForm() {
+    return (
+        `
+            <div class="task-form-container">
+                <form id="task-form">
+                    <div class="field-container">    
+                        <input type="text" id="tkn" class="task-field" spellcheck="false" placeholder="Task">
+                        <input type="text" id="tkd" class="task-field" spellcheck="false" placeholder="Description">
+                    </div>
+                    <div class="task-btn-container">
+                        <div class="task-btn-wrapper">    
+                            <img src="${dateIcon}" id="date-icon" class="btn-icon"> 
+                            <input type="button" id="date-btn" class="task-btn" value="Date">
+                        </div>    
+                        <div class="task-btn-wrapper">
+                            <img src="${priorityIcon}" id="priority-icon" class="btn-icon"> 
+                            <input type="button" id="priority" class="task-btn" value="Priority">
+                        </div>
+                        <div class="task-btn-wrapper">
+                            <img src="${assigneeIcon}" id="assignee-icon" class="btn-icon">
+                            <input type="button" id="assignee-btn" class="task-btn" value="Assignee">
+                        </div>
+                    </div>  
+                    <div class="control-btn-container">
+                        <input type="button" id="cancel-btn" class="control-btn" value="Cancel">
+                        <input type="button" id="form-add-task-btn" class="control-btn" value="Add task">
+                    </div>  
+                </form>
+            </div>  
+        `
+    );
+}
